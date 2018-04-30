2836ff70c56b72d5abdcd491d0fb286051571370
sriganeshharitz@gmail.com <sriganeshharitz@gmail.com>
2018-04-30 09:39:12 +0530

    change brand icon

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var fs = require("fs");
+ 
+http.createServer(function(request, response) {
+}).listen(3000);
+
+fs.readFile("index.html", function(err, data){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(data);
+    response.end();
+  });
